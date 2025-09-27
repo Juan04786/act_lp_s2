e3148e2df3c4291f985a4f7ba365220357bb91c7
Juan04786 <jcosorio768@gmail.com>
2025-09-27 14:31:25 -0500

    Update Ejercicio5.java

--- src/main/java/com/example/Ejercicio5.java
+++ src/main/java/com/example/Ejercicio5.java
@@ -1,8 +1,18 @@
-package com.example;
+
 
 public class Ejercicio5 {
     public static void ejercicio5() {
         System.out.println("Ejercicio 5");
         // tu código aquí
+        System.out.print("Hola ");
+
+        System.out.println("Mundo ");
+
+        int edad = 30;
+        System.out.printf("Mi edad es %d ", edad);
+
+        System.out.println();
+    }
+}
     }
 }
